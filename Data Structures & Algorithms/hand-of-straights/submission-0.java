f240d44ecb48a6d298728210dee66520e058810e
Fangyu3 <32600876+fangyu3@users.noreply.github.com>
2026-05-28 21:41:53 -0400

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        // store number:count in mapping
+        // for each group, start with the smallest number and devrement count
+        // see if the next +1 number exists
+        // loop until all groups are satisfied
+
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        TreeMap<Integer, Integer> mapping = new TreeMap<>();
+        for (int n:hand) {
+            mapping.put(n, mapping.getOrDefault(n,0)+1);
+        }
+
+        int numGroups = hand.length/groupSize;
+
+        for (int i=0; i<numGroups; i++) {
+            int num = mapping.firstKey();
+            int count = mapping.get(num)-1;
+            if (count == 0) {
+                mapping.remove(num);
+            } else {
+                mapping.put(num, count);
+            }
+            for (int j=1; j<groupSize; j++) {
+                num = num+1;
+                count = mapping.getOrDefault(num,-1);
+
+                if (count == -1) {
+                    return false;
+                }
+
+                count = count-1;
+                if (count == 0) {
+                    mapping.remove(num);
+                } else {
+                    mapping.put(num, count);
+                }
+            }
+        }
+
+        return true;
+    }
+    
+}
